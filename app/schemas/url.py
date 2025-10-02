0b2fecab230cbd377a91a401643fa9bee25862db
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-10-02 15:17:44 +0200

    store schema original url as HttpUrl

--- app/schemas/url.py
+++ app/schemas/url.py
@@ -1,16 +1,17 @@
 from pydantic import BaseModel
 from datetime import datetime
+from pydantic.networks import HttpUrl
 
 class CreateUrlRequest(BaseModel):
-    original_url: str
+    original_url: HttpUrl
 
 class CreateUrlResponse(BaseModel):
     id: int
-    original_url: str
+    original_url: HttpUrl
     short_url: str
 
 class GetUrlInfoResponse(BaseModel):
-    original_url: str
+    original_url: HttpUrl
     short_url: str
     clicks: int
     created_at: datetime
